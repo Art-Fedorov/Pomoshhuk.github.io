4caf9728a31db3797867370d91e44c152533efe7
pomoshhuk <pomooshhuk@gmail.com>
2016-09-22 11:30:40 +0300

    commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(function()
 {
+
   $(".flexisel").flexisel({
     visibleItems: 3,
     itemsToScroll: 3,
@@ -29,17 +30,8 @@ $(function()
       }
     }
   });
-  function addwow(boxClass,animateClass,offset=0,duration='1s',delay='.3s'){
-    var wow = new WOW(
-    {
-      boxClass:     boxClass,   
-      animateClass: 'animated '+animateClass, 
-      offset:offset,
-      scrollContainer: null 
-    });
-    $('boxClass').data('wow-duration',duration).data(delay,'2s');
-    wow.init();
-  }
+  
+
 	function parallax(selector,height=0){
     if (height!=0) $(selector).css("height",height);
   }
@@ -51,9 +43,58 @@ $(function()
     resizeParallax();
   });
 
-  
-  addwow("features__item","zoomIn");
-  addwow("plan-item","fadeInRight",100,"2s");
-  addwow("blog-item","slideInLeft",0,'1s');
 
-})
+    //Позволяет реанимировать блоки
+    (function($) {
+    $.fn.animated = function(inEffect, outEffect) {
+        $(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+            if (dir === "down") {
+                $(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+            } else {
+                $(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+            };
+        }, {
+            offset: "95%"
+        }).waypoint(function(dir) {
+            if (dir === "down") {
+                $(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+            } else {
+                $(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+            };
+        }, {
+            offset: -$(window).height()
+        });
+    };
+})(jQuery);
+     
+
+  function duration(dur){
+    var a={
+      "animation-duration":dur,
+      "-webkit-animation-duration":dur
+    };
+    return a;
+  };
+  function delay(dur){
+    var a={
+      "-webkit-animation-delay": dur,
+      "-o-animation-delay": dur,
+      "animation-delay": dur
+    }
+    return a;
+  };
+  $('.employee-skills table tr').each(function(key,val) {
+    var x=$(val).find("td:nth-child(3)").text();
+    console.log($(this).find("td:nth-child(2) div.inner").css('width'));
+    $(this).find("td:nth-child(2) div.inner").css('width',x);
+  });
+
+  $(".features__item").css(duration('.7s')).css(delay('0')).animated("zoomIn",'zoomOut');
+  $(".plan-item").animated("fadeInRight",'fadeOutRight');
+  $(".blog-item").animated("slideInLeft",'slideOutRight');
+  $('.header__text').animated('slideInLeft','fadeOutRight');
+  $('.header__hr').animated('slideInRight','fadeOutLeft');
+  $('.about .header-title+.paragraph').animated('fadeInUp','fadeOutDown');
+  $('.about .table__cell:nth-child(3n-2)').animated('slideInLeft','fadeOutRight');
+  $('.about .table__cell:nth-child(3n)').animated('slideInRight','fadeOutLeft');
+});
