ead5f36217c4ac9e19dab348981467653f12a0d0
pomoshhuk <pomooshhuk@gmail.com>
2016-09-26 00:04:43 +0300

    -commit

--- js/common.js
+++ js/common.js
@@ -28,7 +28,28 @@ $(document).ready(function() {
     slidesToScroll: 4,
     appendArrows: $('.popular .arrows'),
     prevArrow:  $('.popular .arrows .arrow-right'), 
-    nextArrow:  $('.popular .arrows .arrow-left')
+    nextArrow:  $('.popular .arrows .arrow-left'),
+    responsive: [
+
+    {
+      breakpoint: 1200,
+      settings: {
+        rows: 1,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+        
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 1
+      }
+    }]
   });
   $('.today .gallery').slick({
     rows: 2,
@@ -36,23 +57,85 @@ $(document).ready(function() {
     slidesToScroll: 4,
     appendArrows: $('.today .arrows'),
     prevArrow:  $('.today .arrows .arrow-right'), 
-    nextArrow:  $('.today .arrows .arrow-left')
+    nextArrow:  $('.today .arrows .arrow-left'),
+    responsive: [
+
+    {
+      breakpoint: 1200,
+      settings: {
+        rows: 1,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 1
+      }
+    }]
   });
   $('.interest .gallery').slick({
     rows: 1,
     slidesToShow: 4,
     slidesToScroll: 4,
+    dots: false,
     appendArrows: $('.interest .arrows'),
     prevArrow:  $('.interest .arrows .arrow-right'), 
-    nextArrow:  $('.interest .arrows .arrow-left')
+    nextArrow:  $('.interest .arrows .arrow-left'),
+    responsive: [
+
+    {
+      breakpoint: 1200,
+      settings: {
+        rows: 1,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 1
+      }
+    }]
   });
    $('.footer .footer-gallery').slick({
     rows: 1,
     slidesToShow: 4,
     slidesToScroll: 4,
+    dots: false,
     appendArrows: $('.footer .arrows'),
     prevArrow:  $('.footer .arrows .arrow-right'), 
-    nextArrow:  $('.footer .arrows .arrow-left')
+    nextArrow:  $('.footer .arrows .arrow-left'),
+    responsive: [
+
+    {
+      breakpoint: 1200,
+      settings: {
+        rows: 1,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 1
+      }
+    }]
   });
 
 
